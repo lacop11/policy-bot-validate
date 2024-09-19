f792217f382dfa6268cc781efe166505db8db3e0
Laco <laco@elevenlabs.io>
2024-09-19 14:25:24 +0200

    Parse flags

--- hook.sh
+++ hook.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-echo "args are" $@
+# Parse arguments. We should get two.
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 --policy-bot-url=https://... <path to .policy.yml>"
+    echo "You probably need to set \`args: ["--policy-bot-url=https://..."]\` in your"
+    echo ".pre-commit-config.yaml file."
+    exit 1
+fi
+# First argument should be the policy bot URL.
+if [[ $1 != --policy-bot-url=* ]]; then
+    echo "First argument should be --policy-bot-url=..."
+    exit 1
+fi
+POLICY_BOT_URL=${1#--policy-bot-url=}
+# Second argument should be the path to the .policy.yml file.
+POLICY_FILE=$2
+
+echo "URL is" $POLICY_BOT_URL
+echo "File is" $POLICY_FILE
+
+# echo "args are" $@
 exit 1
