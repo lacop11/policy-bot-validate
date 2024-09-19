1e929925f581b4d80f89ce326acd6c989f2e641e
Laco <laco@elevenlabs.io>
2024-09-19 14:29:44 +0200

    Actual validation logic

--- hook.sh
+++ hook.sh
@@ -20,5 +20,21 @@ POLICY_FILE=$2
 echo "URL is" $POLICY_BOT_URL
 echo "File is" $POLICY_FILE
 
-# echo "args are" $@
-exit 1
+# Validate.
+tmpfile=$(mktemp /tmp/policy.XXXXXX)
+code=$( \
+    curl "${POLICY_BOT_URL}/api/validate" \
+    -XPUT \
+    -T "${POLICY_FILE}" \
+    -s -w "%{http_code}" \
+    -o "${tmpfile}" \
+)
+
+if [[ "${rcode}" -gt 299 ]];
+then
+    echo "Validation failed with status code ${rcode}"
+    cat /tmp/response
+    exit 1
+fi
+
+echo "Validation successful"
